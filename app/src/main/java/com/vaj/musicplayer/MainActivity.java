5e5c9a0a967e71df7c6de3328cc46fd8dfb9f680
VikashAnandJha <mailtovikashjha@gmail.com>
2022-08-02 02:42:00 +0530

    Initial commit

---
+++ app/src/main/java/com/vaj/musicplayer/MainActivity.java
@@ -0,0 +1,96 @@
+package com.vaj.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.media.TimedText;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+ImageView playBtn,pauseBtn;
+TextView lapsedTimeTv,totalTimeTv;
+SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer=MediaPlayer.create(this,R.raw.song);
+        playBtn=findViewById(R.id.playBtn);
+        pauseBtn=findViewById(R.id.pauseBtn);
+        lapsedTimeTv=findViewById(R.id.lapsedTimeTv);
+        totalTimeTv=findViewById(R.id.totalTimeTv);
+        seekBar=findViewById(R.id.seekBar);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                playBtn.setVisibility(View.GONE);
+                pauseBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+                pauseBtn.setVisibility(View.GONE);
+                playBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        totalTimeTv.setText(mediaPlayer.getDuration()/1000/60+":"+mediaPlayer.getDuration()/1000%(60));
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(mediaPlayer.isPlaying())
+                    lapsedTimeTv.setText(mediaPlayer.getCurrentPosition()/1000/60+":"+mediaPlayer.getCurrentPosition()/1000%(60));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+
+
+                //.setProgress(mMediaPlayer.getCurrentPosition());
+            }
+        },0,1000);
+
+
+
+
+
+
+
+       //
+
+
+
+    }
+}
